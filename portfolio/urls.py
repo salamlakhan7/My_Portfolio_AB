503e8402f8f58b516e5722be9f356dc85b89c505
salam lakhan <119106199+salamlakhan7@users.noreply.github.com>
2026-03-12 23:09:34 +0500

    Update URL path for profile inclusion

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('profil.urls')),
+    path('profil/', include('profil.urls')),
 ]
